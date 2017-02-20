538b17d847df5bebbda7cb724246fc8654984122
Robert Belson <rhbelson@gmail.com>
2017-02-19 21:27:45 -0600

    todolist dynamically displays text

--- todolist.js
+++ todolist.js
@@ -19,26 +19,61 @@ window.onload=function() {
   st4.addEventListener("click",start_task4);
   st5.addEventListener("click",start_task5);
   st6.addEventListener("click",start_task6);
+  document.getElementById('input_box1').style.visibility="hidden";
+  document.getElementById('input_box2').style.visibility="hidden";
+  document.getElementById('input_box3').style.visibility="hidden";
+  document.getElementById('input_box4').style.visibility="hidden";
+  document.getElementById('input_box5').style.visibility="hidden";
+  document.getElementById('input_box6').style.visibility="hidden";
+  document.getElementById('b1').style.visibility="hidden";
+  document.getElementById('b2').style.visibility="hidden";
+  document.getElementById('b3').style.visibility="hidden";
+  document.getElementById('b4').style.visibility="hidden";
+  document.getElementById('b5').style.visibility="hidden";
+  document.getElementById('b6').style.visibility="hidden";
+ 
 
 
 
 
   t_1=localStorage.getItem('t1');
+  if (t_1) {
+  document.getElementById('input_box1').style.visibility="visible";
   document.getElementById('input_box1').innerHTML=t_1;
+  document.getElementById('b1').style.visibility="visible";
+}
   
   t_2=localStorage.getItem('t2');
+  if (t_2) {
+  document.getElementById('input_box2').style.visibility="visible";
   document.getElementById('input_box2').innerHTML=t_2;
+  document.getElementById('b2').style.visibility="visible";
+}
 
   t_3=localStorage.getItem('t3');
+  if (t_3) {
+  document.getElementById('input_box3').style.visibility="visible";
   document.getElementById('input_box3').innerHTML=t_3;
+  document.getElementById('b2').style.visibility="visible";
+}
   t_4=localStorage.getItem('t4');
+  if (t_4) {
+  document.getElementById('input_box4').style.visibility="visible";
   document.getElementById('input_box4').innerHTML=t_4;
+  document.getElementById('b2').style.visibility="visible";
+  }
+  
 
   t_5=localStorage.getItem('t5');
+  if (t_5) {
   document.getElementById('input_box5').innerHTML=t_5;
+}
 
   t_6=localStorage.getItem('t6');
+  if (t_6) {
   document.getElementById('input_box6').innerHTML=t_6;
+}
+
 }
 
 
